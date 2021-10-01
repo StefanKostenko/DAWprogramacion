f189ee12fa00daf4d45ffa29fa27067acb315184
StefanKostenko <kostenkostephan@gmail.com>
2021-10-01 10:06:52 +0200

    Ejemplo13 en proceso + Ayuda13 para probar cosas

---
+++ Tema 1/Ejemplo13.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Ejemplo13 {
+    public static void main(String[] args) {
+        int numero1;
+        int numero2;
+        String orden;
+
+        System.out.println("Escribeme el primer numero : ");
+        Scanner valorEntrante;
+        valorEntrante = new Scanner(System.in);
+        numero1 = valorEntrante.nextInt();
+
+        System.out.println("Escribeme el segundo numero : ");
+        Scanner valorEntrante2;
+        valorEntrante2 = new Scanner(System.in);
+        numero2 = valorEntrante2.nextInt();
+
+        System.out.println("Escribeme si quieres ver el orden delos numeros ascendente o descendente : ");
+        Scanner valorEntrante3;
+        valorEntrante3 = new Scanner(System.in);
+        orden = valorEntrante3.nextLine();
+
+        valorEntrante.close();
+        valorEntrante2.close();
+        valorEntrante3.close();
+
+        if("ascendente".equals(orden)){
+            if(numero1 <= numero2){
+                System.out.println(" " + numero1 + " , " + numero2);
+            }else if("ascendente".equals(orden)){
+                if(numero1 >= numero2){
+                System.out.println(" " + numero2 + " , " + numero1);
+                }else if("descendente".equals(orden)){
+                        if(numero1 >= numero2){
+                            System.out.println(" " + numero1 + " , " + numero2);
+                        }else if("descendente".equals(orden)){
+                            if(numero1 <= numero2){
+                                System.out.println(" " + numero2 + " , " + numero1);
+                            }
+                    }
+                }
+            }
+        }else{
+            System.out.println("Error!");
+        }
+
+    }
+}
