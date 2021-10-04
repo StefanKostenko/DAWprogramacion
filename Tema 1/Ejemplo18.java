31ed9fc3a6e6a3a897e91dd3fecdd0d2ce6875f7
StefanKostenko <kostenkostephan@gmail.com>
2021-10-04 09:20:55 +0200

    Añadido el caso default al Ejemplo 17

---
+++ Tema 1/Ejemplo18.java
@@ -0,0 +1,18 @@
+public class Ejemplo18 {
+    public static void main(String[] args) {
+        
+        int horas;
+        int minutos;
+        int segundos;
+
+        System.out.println("Dame el valor de horas: ");
+        Scanner valorEntrante;
+        valorEntrante = new Scanner(System.in);
+        horas = valorEntrante.nextLine();
+
+        System.out.println("Dame el valor de minutos: ");
+        Scanner valorEntrante2;
+        valorEntrante = new Scanner(System.in);
+        mes = valorEntrante2.nextLine();
+    }
+}
