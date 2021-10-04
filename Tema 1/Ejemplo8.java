4485aeff6265d73b016505014a13a3c51837ff20
StefanKostenko <kostenkostephan@gmail.com>
2021-10-04 09:49:57 +0200

    Corregido el Scanner Ejemplo8

--- Tema 1/Ejemplo8.java
+++ Tema 1/Ejemplo8.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 public class Ejemplo8 {
     public static void main(String[] args) {
         int edad;
-
-        System.out.println("Escribeme tu edad : ");
         Scanner valorEntrante;
         valorEntrante = new Scanner(System.in);
+
+        System.out.println("Escribeme tu edad : ");
         edad = valorEntrante.nextInt();
+        
         valorEntrante.close();
 
         if(edad >= 18){
