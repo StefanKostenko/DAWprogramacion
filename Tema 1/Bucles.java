0c958fb7e15c4cbcb83549cf1875e06f23068334
StefanKostenko <kostenkostephan@gmail.com>
2021-10-11 09:45:53 +0200

    Bucles explicadops

---
+++ Tema 1/Bucles.java
@@ -0,0 +1,23 @@
+public class Bucles {
+    public static void main(String[] args) {
+         int i;
+
+         for (i = 0; i < 10; i++) {
+             System.out.println("Hola: " + i);
+         }
+         
+         i = 0;
+
+         while(i < 10){
+            System.out.println("Hola: " + i);
+            i++;
+         }
+
+         i = 0;
+
+         do {
+             System.out.println("Hola: " + i);
+             i++;
+         } while (i < 10);
+    }
+}
