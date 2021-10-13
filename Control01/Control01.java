9f8f66f2e59c833aa0b5aa4e78e49ec5587b7f98
StefanKostenko <kostenkostephan@gmail.com>
2021-10-13 09:27:14 +0200

    Terminado el Control01

---
+++ Control01/Control01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Control01 {
+    public static void main(String[] args) {
+        
+        int ladosDePoligono;
+        double longitudDelLadoDelPoligono;
+        Scanner escaner;
+        escaner = new Scanner(System.in);
+
+        System.out.println("Dame de lados del poligono: ");
+        ladosDePoligono = escaner.nextInt();
+
+        if(ladosDePoligono >= 3 && ladosDePoligono <= 4){
+            System.out.println("Dime la longitud del lado del polígono (con decimales): ");
+            longitudDelLadoDelPoligono = escaner.nextDouble();
+
+            System.out.println("El perimetro es: " + ladosDePoligono * longitudDelLadoDelPoligono);
+        }else{
+            System.out.println("Usted ha introducido mas o menos lados del poligono de los que se permite!");
+        }
+
+        escaner.close();
+
+    }
+}
--- Tema 1/Ejemplo25.java
+++ Tema 1/Ejemplo25.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 public class Ejemplo25 {
-    
+    public static void main(String[] args) {
+        
+        int numero;
+        Scanner numeroDeUsuario;
+        numero = new Scanner(System.in);
+
+        for (int i = 0; i < numero; i++) {
+            
+        }
+        
+    }
 }
 
