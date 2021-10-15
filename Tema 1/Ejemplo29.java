5e65fed1aa0cc6b0ec9aeaaf67643d5b43e63ded
StefanKostenko <kostenkostephan@gmail.com>
2021-10-15 10:42:56 +0200

    Terminado el Ejemplo29

--- Tema 1/Ejemplo29.java
+++ Tema 1/Ejemplo29.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
-
 public class Ejemplo29 {
     public static void main(String[] args) {
         
-        int numeroDeUsuario, divisor = 2;
+        int divisor = 2;
         boolean esPrimo = true;
-        Scanner escaner;
-        escaner = new Scanner(System.in);
 
-        System.out.println("Escribeme un numero para saber si es primo o no : ");
-        numeroDeUsuario = escaner.nextInt();
+        for (int i = 3; i <= 100; i++) {
+            divisor = 2;
+            esPrimo = true;
 
-        escaner.close();    
+            while(divisor < i && esPrimo ){
+                if(i % divisor == 0){
+                    esPrimo = false;
+                }
 
-        for (int i = 0; i < args.numeroDeUsuario; i++) {
-            
+                divisor++;
+
+            }
+            if(esPrimo){
+                System.out.println(i);
+            }
+
         }
-
     }
 }
