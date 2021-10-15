f1fe669a91e0e43a0fcd82ac6dd856b8f1070c8d
StefanKostenko <kostenkostephan@gmail.com>
2021-10-15 08:41:07 +0200

    Ejemplo27 terminado

--- Tema 1/Ejemplo27.java
+++ Tema 1/Ejemplo27.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
 public class Ejemplo27 {
-    
+    public static void main(String[] args) {
+        
+        int numeroDeUsuario;
+        String lista = "";
+        Scanner escaner;
+        escaner = new Scanner(System.in);
+
+        System.out.println("Escribeme un numero : ");
+        numeroDeUsuario = escaner.nextInt();
+        numeroDeUsuario ++;
+
+        escaner.close();    
+
+        for (int i = 1; i < numeroDeUsuario; i++) {
+            lista = lista + " " + String.valueOf(i);
+            System.out.println(lista);
+        }      
+    }
 }
+
