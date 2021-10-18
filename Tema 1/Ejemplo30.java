1bcebad504ce10626840aee5f5dc52766e13e369
StefanKostenko <kostenkostephan@gmail.com>
2021-10-18 08:52:00 +0200

    Corregido los impuestos en el Ejemplo19

---
+++ Tema 1/Ejemplo30.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Ejemplo30 {
+    public static void main(String[] args) {
+        
+        String palabraUsuario;
+        Scanner escaner;
+        escaner = new Scanner(System.in);
+
+        System.out.println("Escribeme una palabra para saber si es ");
+    }
+}
