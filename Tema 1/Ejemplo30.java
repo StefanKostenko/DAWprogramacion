0f6ecab1d8e6648a948f55bcd18a0ed0f51e8d9a
StefanKostenko <kostenkostephan@gmail.com>
2021-10-20 09:40:13 +0200

    Terminado el Ejemplo 30

--- Tema 1/Ejemplo30.java
+++ Tema 1/Ejemplo30.java
@@ -1,12 +1,36 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Ejemplo30 {
     public static void main(String[] args) {
         
+        //Declaracion de variables
+
         String palabraUsuario;
+        int longitud;
+        double laMitadDeLaLongitud;
+        boolean palindromo = true;
         Scanner escaner;
         escaner = new Scanner(System.in);
 
-        System.out.println("Escribeme una palabra para saber si es ");
+        System.out.println("Escribeme una palabra para saber si es palíndroma: ");
+        palabraUsuario = escaner.nextLine();
+
+        //Damos el valor a la longitud que es el numero de letras que introduce el usuario.
+        //Y la mitad de la longi
+
+        longitud = palabraUsuario.length();
+        laMitadDeLaLongitud = longitud/2;
+
+        for (int i = 1; i < laMitadDeLaLongitud; i++) {
+            if(palabraUsuario.charAt(i) != palabraUsuario.charAt(longitud - i)){
+            palindromo = false;
+            }
+        }
+
+        if(palindromo == true){
+            System.out.println("La palabra es palindroma!");
+        } else if(palindromo == false){
+            System.out.println("La palabra no es palindroma!");
+        }
     }
 }
