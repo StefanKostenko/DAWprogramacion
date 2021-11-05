12b3dcbde195ed45778bce91b8a8d79c890610ac
StefanKostenko <kostenkostephan@gmail.com>
2021-11-05 10:13:12 +0100

    Terminado el E05

--- Controles/Control 02/E05.java
+++ Controles/Control 02/E05.java
@@ -3,19 +3,18 @@ public class E05 {
     public static void main(String[] args) {
         
         int numero;
-        String invertido1, invertido2, invertido3;
+        char invertido1, invertido2, invertido3;
         Scanner lector;
         lector = new Scanner(System.in);
 
         System.out.println("Ingrese numero: ");
         numero = lector.nextInt();
 
-        String numeroString = numero+"";
+        String numeroString = numero + "";
 		
-        invertido1 = numeroString.substring(numeroString.charAt(2));
-        invertido2 = numeroString.substring(numeroString.charAt(2));
-        invertido3 = numeroString.substring(numeroString.charAt(1));
-        System.out.println(invertido1 + invertido2 + invertido3);
-        
+        invertido1 = numeroString.charAt(2);
+        invertido2  = numeroString.charAt(1);
+        invertido3  = numeroString.charAt(0);
+        System.out.println(invertido1 + "" +  invertido2 + "" + invertido3);
     }
 }
