891dec073aa08cfc1105b2bef46638fd9a1ce937
StefanKostenko <kostenkostephan@gmail.com>
2021-11-05 09:59:39 +0100

    Dejado a mitad el E05

--- Controles/Control 02/E05.java
+++ Controles/Control 02/E05.java
@@ -1,3 +1,21 @@
+import java.util.*;
 public class E05 {
-    
+    public static void main(String[] args) {
+        
+        int numero;
+        String invertido1, invertido2, invertido3;
+        Scanner lector;
+        lector = new Scanner(System.in);
+
+        System.out.println("Ingrese numero: ");
+        numero = lector.nextInt();
+
+        String numeroString = numero+"";
+		
+        invertido1 = numeroString.substring(numeroString.charAt(2));
+        invertido2 = numeroString.substring(numeroString.charAt(2));
+        invertido3 = numeroString.substring(numeroString.charAt(1));
+        System.out.println(invertido1 + invertido2 + invertido3);
+        
+    }
 }
