0aba99122389808bf8fb84e38f83ee54dd444e76
StefanKostenko <kostenkostephan@gmail.com>
2021-11-05 10:24:05 +0100

    Terminado el E06

--- Controles/Control 02/E06.java
+++ Controles/Control 02/E06.java
@@ -1,3 +1,20 @@
+import java.util.*;
 public class E06 {
-    
+    public static void main(String[] args) {
+        double catetoA, catetoB, hipotenusa;
+        Scanner lector;
+        lector = new Scanner(System.in);
+
+        System.out.print("Ingrese cateto a: ");
+        catetoA = lector.nextDouble();
+
+        System.out.print("Ingrese cateto b: ");
+        catetoB = lector.nextDouble();
+
+        hipotenusa = (catetoA * catetoA) + (catetoB *catetoB);
+        hipotenusa  = Math.sqrt(hipotenusa);
+
+        System.out.println("La hipotenusa es " + hipotenusa);
+        lector.close();
+    }
 }
