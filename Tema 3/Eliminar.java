6b3e1f39b1ca01992376a40919ab43b608e72b84
StefanKostenko <kostenkostephan@gmail.com>
2021-11-26 10:45:51 +0100

    Corregido el ejercico Eliminar

--- Tema 3/Eliminar.java
+++ Tema 3/Eliminar.java
@@ -1,22 +1,17 @@
 public class Eliminar {
     public static void main(String[] args) {
-        int ValorEliminado = Utilidades.leerEntero("Dame un numero para eliminar un elemento del array");
-        int[] array ={10,12,14,16,18};
 
-        if (ValorEliminado > array.length) {
-            do{
-                ValorEliminado = Utilidades.leerEntero("El valor introducido es mayor al admitido");
-            }while(ValorEliminado > array.length);   
+        //int [] numeros = new int[10];
+
+        //Utilidades.rellenaArray(numeros, 1, 10);
+
+        int [] numeros = {1,2,3,4,4,3,2,6,9};
+
+        int indice = 2;
+
+        for (int i = indice + 1; i < numeros.length; i++) {
+            numeros[i - 1] = numeros[i];
         }
-    
-        array[ValorEliminado] = array[4];
-        array[4]=0;
-        
-
-
-        for (int i=0; i < array.length; i++) {
-            System.out.print(array[i] + ",");
-        }
-
+        numeros[numeros.length - 1] = 0;
     }
 }
