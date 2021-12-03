808e7f7af9f171bef23c90593fec416b2a5eaf22
StefanKostenko <kostenkostephan@gmail.com>
2021-12-03 08:37:39 +0100

    MoverCeros Terminado

--- Tema 3/MoverCeros.java
+++ Tema 3/MoverCeros.java
@@ -1,24 +1,20 @@
 public class MoverCeros {
     public static void main(String[] args) {
         int[] numeros = {0,1,2,3,0,5};
-        int contador = 0;
+        int [] numerosCopia = new int[numeros.length];
+        int j = 0;
 
-        for (int i = ; i < numeros.length; i++) {
-            if(numeros[i] == 0){
-               contador++;
+        for (int i = 0; i < numerosCopia.length; i++) {
+            if(numeros[i] != 0){
+                numerosCopia[j] = numeros[i];
+                j++;
             }
         }
-        Utilidades.mostrarArray(numeros);
+        
+        numeros = numerosCopia;
 
-        public static void eliminarNumero(int[] numeros, int indice) {
-            int numeroBorrado = numeros[indice];
-    
-            for (int i = indice + 1; i < numeros.length; i++) {
-                numeros[i - 1] = numeros[i];
-            }
-            numeros[numeros.length - 1] = numeroBorrado;
-        }
+        Utilidades.mostrarArray(numerosCopia);
+
     }
 }
 
-// cambiar el orden de drecha a izquiera en el for
