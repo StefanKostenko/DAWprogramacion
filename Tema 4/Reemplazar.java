83da475cb13bf78fdfa9b9f243becb31eedb8df6
StefanKostenko <kostenkostephan@gmail.com>
2021-12-13 09:14:31 +0100

    Reemplazar terminadao

---
+++ Tema 4/Reemplazar.java
@@ -0,0 +1,10 @@
+public class Reemplazar {
+    public static void main(String[] args) {
+        String cadena = "Hola", cadenaConCambios;
+
+        cadenaConCambios = cadena.replace("o", "a");
+
+        System.out.println(cadenaConCambios);
+
+    }
+}
