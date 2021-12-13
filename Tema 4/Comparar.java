0be296cf342e8c4bd42f68dcb99f7a19bde1362b
StefanKostenko <kostenkostephan@gmail.com>
2021-12-13 08:40:06 +0100

    Corregido la expresion logica de Comparar

--- Tema 4/Comparar.java
+++ Tema 4/Comparar.java
@@ -8,9 +8,9 @@ public class Comparar {
 
         if(compararCadenas == 0){
             System.out.println("La cadena 1 es igual a la 2");
-        } else if(compararCadenas == -1){
+        } else if(compararCadenas < 0){
             System.out.println("La cadena 2 es mas grande que la 1");
-        } else if(compararCadenas == 1){
+        } else if(compararCadenas > 0){
             System.out.println("La cadena 1 es mas grande que la 2");
         }
 
