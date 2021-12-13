67df00c8d5619711365c4477ed26d7355a1e0a79
StefanKostenko <kostenkostephan@gmail.com>
2021-12-13 09:32:31 +0100

    Substring terminado

---
+++ Tema 4/Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+    public static void main(String[] args) {
+        String cadena = "Hola Mundo", cadenaSubString;
+
+        cadenaSubString = cadena.substring(0, 4);
+        System.out.println(cadenaSubString);
+    }
+}
