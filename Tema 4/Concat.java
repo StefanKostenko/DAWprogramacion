0fc611d7ebcbe2e60a9b7d5db4c1104f0a821b12
StefanKostenko <kostenkostephan@gmail.com>
2021-12-13 08:50:30 +0100

    Concat terminado

---
+++ Tema 4/Concat.java
@@ -0,0 +1,8 @@
+public class Concat {
+    public static void main(String[] args) {
+        String cadena1 = "Hola", cadena2 = "Mundo";
+
+        String cadena3 = cadena1.concat(cadena2);
+        System.out.println(cadena3);
+    }
+}
