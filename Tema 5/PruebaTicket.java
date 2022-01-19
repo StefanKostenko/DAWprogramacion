9a84e6daad8118e7007382394ac1033bc0d3eb6e
StefanKostenko <kostenkostephan@gmail.com>
2022-01-19 13:06:34 +0100

    Ticket2 ejercico 1 acabado

--- Tema 5/PruebaTicket.java
+++ Tema 5/PruebaTicket.java
@@ -1,9 +1,10 @@
 public class PruebaTicket {
     public static void main(String[] args) {
-        TicketMachine t1 = new TicketMachine();
-        t1.setPrice(72);
+        TicketMachine t1 = new TicketMachine(100);
         t1.insertMoney(100);
+        t1.insertMoney(200);
         t1.printTicket();
+        t1.showPrice();
         t1.empty();
         t1.showPrice();
     }
