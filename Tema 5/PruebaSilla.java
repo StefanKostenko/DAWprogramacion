60a205af63834fb70bbb91f8f4fa87ce448f18ab
StefanKostenko <kostenkostephan@gmail.com>
2022-01-19 11:17:26 +0100

    Tema 5 creado y Silla terminado

---
+++ Tema 5/PruebaSilla.java
@@ -0,0 +1,12 @@
+import java.awt.Point;
+
+public class PruebaSilla {
+    public static void main(String[] args) {
+        
+        Silla [] sillas = new Silla[100];
+
+        for (int i = 0; i < sillas.length; i++) {
+            sillas[i] = new Silla();
+        }
+    }
+}
---
+++ Tema 5/Silla.java
@@ -0,0 +1,163 @@
+import java.awt.Point;
+
+public class Silla {
+
+    private static String [] colores = {"azul " , "rojo " , "amarillo " , "negro "};
+    private static String [] materiales = {"madera " , "metal " , "plastico "}; 
+
+    // atributos de silla
+
+    private String color;
+    private int numeroDePatas;
+    private boolean respaldo;
+    private int comodidad;
+    private Point cordenadas;
+    private String material;
+
+    public Silla(){
+        // Generar silla aleatoria
+        this.color = Silla.colorAleatorio();
+        this.material = Silla.materialAleatorio();
+        this.numeroDePatas = Silla.numPatasAleatorio();
+        this.respaldo = Silla.respaldoAleatorio();
+        this.comodidad = Silla.comodidadAleatorio();
+        this.cordenadas = Silla.cordenadasAleatorio();
+    }
+
+    public Silla(String color, int numeroDePatas, boolean respaldo, int comodidad, Point cordenadas, String material) {
+        this.color = color;
+        this.numeroDePatas = numeroDePatas;
+        this.respaldo = respaldo;
+        this.comodidad = comodidad;
+        this.cordenadas = cordenadas;
+        this.material = material;
+    }
+
+    public static Point cordenadasAleatorio(){
+        Point punto = new Point();
+
+        int min = 0;
+        int max = 100;
+
+        int x = (int) (Math.random() * (max - min + 1) + min);
+        int y = (int) (Math.random() * (max - min + 1) + min);
+
+        punto.setLocation(x, y);
+        return punto;
+    }
+
+    public static boolean respaldoAleatorio(){
+        int min = 0, max = 4;
+
+        int aleatorio = (int) (Math.random() * (max - min + 1) + min);
+
+        if(aleatorio == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public static int numPatasAleatorio(){
+        int min = 0, max = 4;
+
+        int aleatorio = (int) (Math.random() * (max - min + 1) + min);
+
+        return aleatorio;
+    }
+
+    public static int comodidadAleatorio(){
+        int min = 0, max = 4;
+
+        int aleatorio = (int) (Math.random() * (max - min + 1) + min);
+
+        return aleatorio;
+    }
+
+	public static String colorAleatorio() {
+		int min = 0;
+        int max = colores.length -1;
+
+        int aleatorio = (int) (Math.random() * (max - min + 1) + min);
+
+        return colores[aleatorio];
+	}
+
+    public static String materialAleatorio() {
+		int min = 0;
+        int max = materiales.length -1;
+
+        int aleatorio = (int) (Math.random() * (max - min + 1) + min);
+
+        return materiales[aleatorio];
+	}
+
+    //Getters
+
+    public int getComodidada() {
+        return comodidad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumeroDePatas() {
+        return numeroDePatas;
+    }
+
+    public String getRespaldo() {
+        if(respaldo){
+            return "Sí";
+        }else{
+            return "No";
+        }
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public Point getCordenadas() {
+        return cordenadas;
+    }
+
+    //Setters
+
+    public void setComodidad(int comodidad) {
+        if(comodidad < 0){
+            this.comodidad = 0;
+        }else if(comodidad > 10){
+            this.comodidad = 10;
+        }else{
+            this.comodidad = comodidad;
+        }
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumeroDePatas(int numeroDePatas) {
+        if(numeroDePatas < 0){
+            this.numeroDePatas = 0;
+        }else if(numeroDePatas > 100){
+            this.numeroDePatas = 100;
+        }else{
+            this.numeroDePatas = numeroDePatas;
+        }
+    }
+
+    public void setRespaldo(boolean respaldo) {
+        this.respaldo = respaldo;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setCordenadas(Point cordenadas) {
+        this.cordenadas = cordenadas;
+    }
+
+}
