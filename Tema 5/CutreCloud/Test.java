e00ed1e202cb4f92834e4a76daeae1377fca3ecd
StefanKostenko <kostenkostephan@gmail.com>
2022-02-25 10:41:58 +0100

    Aplicacion terminado

--- Tema 5/CutreCloud/Test.java
+++ Tema 5/CutreCloud/Test.java
@@ -8,11 +8,20 @@ public class Test {
         Usuario u3 = new Usuario(124, "gfh@tata.com", "dfjgl");
         Usuario u4 = new Usuario(123, "gaga@gmail.com", "asdfas");
 
-        Media m1 = new Media(2346789, "Antonio.jpg", "porno", MediaType.IMAGEN, u1);
+        Media m1 = new Media(2346789, "Antonio.jpg", "imagen", MediaType.IMAGEN, u1);
 
         Media m2 = new Media(2346789, "Borja.mp3", "cancion", MediaType.AUDIO, u2);
 
 
-        Media m3 = new Media(2346789, "Antonio.jpg", "porno", MediaType.IMAGEN, u1);
+        Media m3 = new Media(2346789, "Antonio.jpg", "imagen", MediaType.IMAGEN, u1);
+
+        Aplicacion a1 = new Aplicacion();
+
+        a1.addUsuario(u1);
+        a1.addFilesMedia(m1);
+        a1.addFilesMedia(m2);
+        a1.addFilesMedia(m3);
+
+        a1.eliminarTodos(MediaType.IMAGEN);
     }
 }
