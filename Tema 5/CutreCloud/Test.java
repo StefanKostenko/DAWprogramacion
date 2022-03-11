1100adec2ff412954a1445a5781ccf10c8d6b3c5
StefanKostenko <kostenkostephan@gmail.com>
2022-03-11 09:06:24 +0100

    main pasado al test

--- Tema 5/CutreCloud/Test.java
+++ Tema 5/CutreCloud/Test.java
@@ -1,10 +1,17 @@
 package CutreCloud;
 
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
         
-    
+
+        PruebaXPath.crearUsuariosDeXML();
 
     }
 
