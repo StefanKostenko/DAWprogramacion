e747a08c2b53507a5d7ff34adfad17d210c9b31b
StefanKostenko <kostenkostephan@gmail.com>
2022-03-14 10:20:11 +0100

    Stat acabada

---
+++ Tema 5/RPG/Character/Stat/Stat.java
@@ -0,0 +1,29 @@
+package RPG.Character.Stat;
+
+public abstract class Stat {
+    private int value;
+
+    public Stat(int value) {
+        this.value = value;
+    }
+
+    //Getters
+    public int getValue() {
+        return value;
+    }
+
+    public void increase(){
+        value++;
+    }
+
+    public void decrease(){
+        value--;
+    }
+
+    @Override
+    public String toString() {
+        return "Stat []";
+    }
+
+
+}
