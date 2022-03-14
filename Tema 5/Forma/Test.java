a8ea744666da10fd3f10a59b85f522c94a3f02b7
StefanKostenko <kostenkostephan@gmail.com>
2022-03-14 09:36:26 +0100

    Bucle creado

--- Tema 5/Forma/Test.java
+++ Tema 5/Forma/Test.java
@@ -1,27 +1,30 @@
 package Forma;
 
+import java.util.ArrayList;
+
 public class Test{
     public static void main(String[] args) {
         Forma f1 = new Forma("negro", 1, 1, "f1");
 
         Rectangulo r1 = new Rectangulo("rojo", 2, 2, "Rectangulo1", 10, 10);
 
-        f1.Imprimir();
-        f1.mover(7, 8);
-        f1.Imprimir();
-        f1.setColor("amarillo");
-        f1.Imprimir();
+        Cuadrado c1 = new Cuadrado("negro", 2, 2, "Cuadrado1", 10);
 
-        r1.Imprimir();
-        r1.mover(2, 2);
-        r1.Imprimir();
-        r1.areaRectangulo();
-        r1.cambiarTamaño(0.5);
-        r1.Imprimir();
-        r1.perímetro();
+        Elipse e1 = new Elipse("azul", 4, 2, "Elipse1", 10, 7);
 
-        Cuadrado c1 = new Cuadrado("negro", 2, 2, "cuadrado", 10);
-        c1.areaCuadrado();
-        c1.Imprimir();
+        Circulo ci1 = new Circulo("blanco", 3, 3, "Circulo1", 5);
+
+        ArrayList<Forma> Objetos = new ArrayList<>();
+
+        Objetos.add(r1);
+        Objetos.add(c1);
+        Objetos.add(e1);
+        Objetos.add(ci1);
+
+        for (Forma itemForma : Objetos) {
+            itemForma.setColor("negro");
+            itemForma.mover(2, 2);
+            itemForma.Imprimir();
+        }
     }
 }
