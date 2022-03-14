75ff190addfc03f3c4023d252a7e1aff0f5dad7c
StefanKostenko <kostenkostephan@gmail.com>
2022-03-14 10:40:36 +0100

    Races

---
+++ Tema 5/RPG/Character/Race/Orc.java
@@ -0,0 +1,13 @@
+package RPG.Character.Race;
+
+import RPG.Character.Stat.Stat;
+
+public class Orc extends Race{
+
+    @Override
+    public int modifier(Stat stat) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
