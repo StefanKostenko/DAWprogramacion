1a7d095ea71eef93b5241d4a7a4623b3512686e2
StefanKostenko <kostenkostephan@gmail.com>
2022-03-16 09:01:28 +0100

    Jobs creados

---
+++ Tema 5/RPG/Character/Job/Assassin.java
@@ -0,0 +1,22 @@
+package RPG.Character.Job;
+
+import RPG.Character.Stat.*;
+import RPG.Character.Stat.Stat;
+
+public class Assassin extends Job{
+
+    int resultado = 0;
+
+    @Override
+    public int modifier(Stat stat) {
+        if(stat instanceof Dexterity){
+            resultado = 3;
+        }else if(stat instanceof Strength){
+            resultado = 1;
+        }else if(stat instanceof Constitution){
+            resultado = 1;
+        }
+        return resultado;
+    }
+    
+}
--- Tema 5/RPG/Character/Job/Mage.java
+++ Tema 5/RPG/Character/Job/Mage.java
@@ -1,18 +1,16 @@
-package RPG.Character.Race;
+package RPG.Character.Job;
 
 import RPG.Character.Stat.Stat;
 import RPG.Character.Stat.*;
 
-public class Human extends Race {
+public class Mage extends Job{
 
     int resultado = 0;
-    
+
     @Override
     public int modifier(Stat stat) {
-        if(stat instanceof Strength){
-            resultado = 2;
-        }else if(stat instanceof Constitution){
-            resultado = 2;
+        if(stat instanceof Intelligence){
+            resultado = 4;
         }else if(stat instanceof Dexterity){
             resultado = 1;
         }
