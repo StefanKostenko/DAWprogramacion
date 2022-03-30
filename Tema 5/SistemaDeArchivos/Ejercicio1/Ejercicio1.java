b5148be8f5ae3dcd9c603f5c4128a2e9c055fc13
StefanKostenko <kostenkostephan@gmail.com>
2022-03-30 10:37:50 +0200

    variable cambiada

--- Tema 5/SistemaDeArchivos/Ejercicio1/Ejercicio1.java
+++ Tema 5/SistemaDeArchivos/Ejercicio1/Ejercicio1.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
 
-    private static File root = File.listRoots()[0];
-    private static File contenido[] = root.listFiles();
+    private static File ruta = File.listRoots()[0];
+    private static File contenido[] = ruta.listFiles();
     private static Scanner lector = new Scanner(System.in);
 
     private static void imprimirContenido() {
 
         int contador = 1;
 
-        if(root.getParent() != null){
-            System.out.println("0. " + root.getParent());
+        if(ruta.getParent() != null){
+            System.out.println("0. " + ruta.getParent());
         }else{
             System.out.println("0. Directorio padre");
         }
@@ -29,20 +29,18 @@ public class Ejercicio1 {
         }
     }
 
-    public static 
-
     public static void main(String[] args) {
         int opcion = 0;
 
         while(opcion != -1){
-            System.out.println("Lista de ficheros y directorios del directorio:" + root);
+            System.out.println("Lista de ficheros y directorios del directorio:" + ruta);
             System.out.println("-------------------------------------------------");
             imprimirContenido();
             System.out.println("Introduce una opcion o (-1 para salir): ");
             opcion = lector.nextInt();
     
             if(opcion <= contenido.length){
-                root = contenido[opcion-1];
+                ruta = contenido[opcion-1];
                 imprimirContenido();
             }
         }
