1cfcc8e259b21f9dde8b5c5b52d7a69191a09f4e
StefanKostenko <kostenkostephan@gmail.com>
2022-04-01 08:58:00 +0200

    File contenido borrado

--- Tema 5/SistemaDeArchivos/Ejercicio1/Ejercicio1.java
+++ Tema 5/SistemaDeArchivos/Ejercicio1/Ejercicio1.java
@@ -5,26 +5,23 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
     private static File ruta = File.listRoots()[0];
-    private static File contenido[] = ruta.listFiles();
     private static Scanner lector = new Scanner(System.in);
 
-    private static void imprimirContenido() {
-        
-        File contenido[] = ruta.listFiles();
+    private static void imprimirContenido(File e) {
 
         int contador = 1;
 
-        if(ruta.getParent() != null){
-            System.out.println("0. " + ruta.getParent());
+        if(e.getParent() != null){
+            System.out.println("0. " + e.getParent());
         }else{
             System.out.println("0. Directorio padre");
         }
 
-        for (int j = 0; j < contenido.length; j++) {
-            if(contenido[j].isDirectory()){
-                System.out.println(contador + ". " + contenido[j] + " <Directorio>");
-            }else if(contenido[j].isFile()){
-                System.out.println(contador + ". " + contenido[j] + " " + contenido[j].length());
+        for (int j = 0; j < e.listFiles().length; j++) {
+            if(e.listFiles()[j].isDirectory()){
+                System.out.println(contador + ". " + e.listFiles()[j] + " <Directorio>");
+            }else if(e.listFiles()[j].isFile()){
+                System.out.println(contador + ". " + e.listFiles()[j] + " " + e.listFiles()[j].length());
             }
 
             contador++;
@@ -32,11 +29,11 @@ public class Ejercicio1 {
     }
 
     public static void main(String[] args) {
+        
         int opcion = 0;
 
-        System.out.println("Lista de ficheros y directorios del directorio:" + ruta);
-        System.out.println("-------------------------------------------------");
-        imprimirContenido();
+        mensajeDeRuta();
+        imprimirContenido(ruta);
         System.out.println("Introduce una opcion o (-1 para salir): ");
         opcion = lector.nextInt();
 
@@ -44,18 +41,23 @@ public class Ejercicio1 {
     
             if(opcion == 0){
                 ruta = ruta.getParentFile();
-                System.out.println("Lista de ficheros y directorios del directorio:" + ruta);
-                System.out.println("-------------------------------------------------");
-                imprimirContenido();
-            }else if(opcion <= contenido.length){
-                ruta = contenido[opcion-1];
-                System.out.println("Lista de ficheros y directorios del directorio:" + ruta);
-                System.out.println("-------------------------------------------------");
-                imprimirContenido();
+                mensajeDeRuta();
+                imprimirContenido(ruta);
+            }else if(opcion <= ruta.listFiles().length){
+                ruta = ruta.listFiles()[opcion-1];
+                mensajeDeRuta();
+                imprimirContenido(ruta);
+            }else if(opcion > ruta.listFiles().length){
+                System.out.println("La opcion que ha elegido no esta dentro del rango!");
             }
             System.out.println("Introduce una opcion o (-1 para salir): ");
             opcion = lector.nextInt();
         }
 
+    }
+
+    private static void mensajeDeRuta() {
+        System.out.println("Lista de ficheros y directorios del directorio:" + ruta);
+        System.out.println("-------------------------------------------------");
     }
 }
