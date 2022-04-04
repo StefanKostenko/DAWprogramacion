b121e762206dc94c3681c26c562dca0792098ea1
StefanKostenko <kostenkostephan@gmail.com>
2022-04-04 09:08:42 +0200

    Plantilla del ejercicio

---
+++ Tema 5/ContenidoDeFicheros/Ejercicio2_1.java
@@ -0,0 +1,23 @@
+package ContenidoDeFicheros;
+
+import java.io.File;
+
+import java.io.IOException;
+
+public class Ejercicio2_1 {
+
+    public static void main(String[] args) throws IOException {
+
+        File f = new File("./penyagolosa.bmp");
+
+        TransformaImagen ti = new TransformaImagen(f);
+
+        ti.transformaNegativo();
+
+        ti.transformaOscuro();
+
+        ti.transformaBlancoNegro();
+
+    }
+
+}
