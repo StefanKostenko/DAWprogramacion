1bb0e064d16fe81012c54004c3eb9b1d7ff82ccc
StefanKostenko <kostenkostephan@gmail.com>
2022-04-06 08:34:38 +0200

    empezado el transformar negativo

--- Tema 5/ContenidoDeFicheros/Ejercicio2_1.java
+++ Tema 5/ContenidoDeFicheros/Ejercicio2_1.java
@@ -1,5 +1,3 @@
-package ContenidoDeFicheros;
-
 import java.io.File;
 
 import java.io.IOException;
@@ -8,15 +6,15 @@ public class Ejercicio2_1 {
 
     public static void main(String[] args) throws IOException {
 
-        File f = new File("./penyagolosa.bmp");
+        File f = new File("penyagolosa.bmp");
 
         TransformaImagen ti = new TransformaImagen(f);
 
-        ti.transformaNegativo();
+        ti.transformaNegativo(f);
 
-        ti.transformaOscuro();
+        //ti.transformaOscuro();
 
-        ti.transformaBlancoNegro();
+        //ti.transformaBlancoNegro();
 
     }
 
--- Tema 5/ContenidoDeFicheros/TransformaImagen.java
+++ Tema 5/ContenidoDeFicheros/TransformaImagen.java
@@ -1,5 +1,3 @@
-package ContenidoDeFicheros;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -14,15 +12,27 @@ public class TransformaImagen {
         // Control de existencia del fichero y control de la extensión .bmp (sacar
         // mensajes de error)
 
-        
+        if(fEnt.exists() == false){    
+            System.out.println("El fichero no existe!");
+        }   
 
 
     }
 
-    public void transformaNegativo() throws IOException {
+    public void transformaNegativo(File file) throws IOException {
 
         // Transformar a negativo y guardar como *_n.bmp
 
+        FileInputStream fNeg_in = new FileInputStream(file);
+        FileOutputStream fNeg_out = new FileOutputStream(getNombreSinExtension(file) + "_n.bmp");
+
+        byte[] buffer = new byte[54];
+        int num = fNeg_in.read(buffer);
+        while(num != -1){
+            fNeg_out.write(buffer,0,num);
+        }
+        fNeg_in.close();
+        fNeg_out.close();
     }
 
     public void transformaOscuro() throws IOException {
@@ -30,7 +40,6 @@ public class TransformaImagen {
         // Transformar a una imagen más oscura y guardar como *_o.bmp
 
         
-
     }
 
     
@@ -41,12 +50,17 @@ public class TransformaImagen {
 
     }
 
-    private String getNombreSinExtension() {
+    private String getNombreSinExtension(File file) {
 
         //Devuelve el nombre del archivo f sin extensión
 
-        
+        String fname = file.getName();
+        int pos = fname.lastIndexOf(".");
+        if (pos > 0) {
+            fname = fname.substring(0, pos);
+        }
 
+        return fname;
     }
 
 }
