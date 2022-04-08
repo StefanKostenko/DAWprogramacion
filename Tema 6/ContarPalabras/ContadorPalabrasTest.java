258b7750af61aa34abdbbed738b4a84af67d7a0d
StefanKostenko <kostenkostephan@gmail.com>
2022-04-08 10:43:45 +0200

    Contar palabras

---
+++ Tema 6/ContarPalabras/ContadorPalabrasTest.java
@@ -0,0 +1,11 @@
+package ContarPalabras;
+
+import java.io.IOException;
+
+public class ContadorPalabrasTest {
+    public static void main(String[] args) throws IOException {
+        ContarPalabras cnp = new ContarPalabras();
+
+        cnp.leerFichero();
+    }
+}
