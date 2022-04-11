a6ff532d04992dbea10960803d8bfd99352d09ba
StefanKostenko <kostenkostephan@gmail.com>
2022-04-11 09:33:01 +0200

    ContadorPalabras cerrado el archivo

---
+++ Tema 6/EliminarComentarios/EliminarComentarios.java
@@ -0,0 +1,10 @@
+package EliminarComentarios;
+
+import java.io.IOException;
+
+public class EliminarComentarios {
+    
+    public void EliminarComentarios() throws IOException{
+        
+    }
+}
