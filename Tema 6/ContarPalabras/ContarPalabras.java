61ff457e4aab0984758a08e17fcb58b2a2eb2805
StefanKostenko <kostenkostephan@gmail.com>
2022-04-11 09:26:19 +0200

    Contador de palabras finalizado

--- Tema 6/ContarPalabras/ContarPalabras.java
+++ Tema 6/ContarPalabras/ContarPalabras.java
@@ -3,36 +3,37 @@ package ContarPalabras;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ContarPalabras {
     
     public void leerFichero() throws IOException{
-        
-        String cadena;
-        ArrayList<String> contenido = new ArrayList<>();
+
+        String palabra;
+        String contenido;
+        int contador = 0;
 
         FileReader archivo = new FileReader("Tema 6/ContarPalabras/palabras.txt");
         BufferedReader br = new BufferedReader(archivo);
 
-        while((cadena = br.readLine())!=null) { 
-             contenido.add(cadena);
-        }
+        Scanner lector = new Scanner(System.in);
 
-        int contador = 1;
-        String palabra = "";
+        System.out.println("Dame una palabra: ");
+        palabra = lector.nextLine();
 
-        for (int i = 0; i < contenido.size(); i++) {
-            palabra = contenido.get(i);
-            contenido.remove(i);
-            for (int j = 0; j < contenido.size(); j++) {
-                if(palabra.equals(contenido.get(j))){
-                    contenido.remove(j);
+        while((contenido = br.readLine())!=null) { 
+            String[] palabras = contenido.split(" ");
+
+            for (int i = 0; i < palabras.length; i++) {
+                if(palabras[i].equals(palabra)){
                     contador++;
                 }
             }
         }
 
-        br.close(); 
+        System.out.println("La palabra " + palabra + " se ha repetido " + contador + " veces.");
+
+        lector.close();
+        br.close();
     }
 }
