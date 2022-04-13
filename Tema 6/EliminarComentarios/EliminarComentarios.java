e64a1582380ae385e60637748025347aa66ece44
StefanKostenko <kostenkostephan@gmail.com>
2022-04-13 09:37:25 +0200

    Eliminar comentarios simples finalizado

--- Tema 6/EliminarComentarios/EliminarComentarios.java
+++ Tema 6/EliminarComentarios/EliminarComentarios.java
@@ -3,6 +3,7 @@ package EliminarComentarios;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class EliminarComentarios {
     
@@ -10,27 +11,37 @@ public class EliminarComentarios {
         
         FileReader archivo = new FileReader("Tema 6/EliminarComentarios/Prueba.java");
         BufferedReader br = new BufferedReader(archivo);
+        PrintWriter pw = new PrintWriter("Tema 6/EliminarComentarios/PruebaSinComentarios.java");
 
-        String contenido;
+        String linia;
+        String liniaSinEspacios;
         String principioDeLinea;
-        int con = 0;
 
-        while((contenido = br.readLine())!= null){
+        while((linia = br.readLine())!= null){
 
-            contenido = contenido.replaceAll("\\s","");
+            liniaSinEspacios = linia.replaceAll("\\s","");
 
-            if(contenido.length() > 1){
-                principioDeLinea = contenido.substring(0, 2);
+            if(liniaSinEspacios.length() > 0){
+
+                if(liniaSinEspacios.length() == 1){
+                    pw.println(linia);
+                }else{
+                    principioDeLinea = liniaSinEspacios.substring(0, 2);
             
-                if(principioDeLinea.equals("//")){
-                    con++;
+                    if(principioDeLinea.equals("//")){
+                    }else{
+                        pw.println(linia);
+                    }
                 }
+            }else{
+                pw.println("");
             }
             
         }
 
-        System.out.println(con);
+        pw.flush();
 
+        pw.close();
         archivo.close();
         br.close();
     }
--- Tema 6/EliminarComentarios/PruebaSinComentarios.java
+++ Tema 6/EliminarComentarios/PruebaSinComentarios.java
@@ -1,9 +1,7 @@
 package EliminarComentarios;
 
 public class Prueba {
-    //Main
     public static void main(String[] args) {
-        //Escribe hola mundo por terminal.
         System.out.println("Hola Mundo!");
 
         System.out.println("Adios!");
