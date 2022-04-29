78c484f9f03b84aba645755ccdfbd93c455f07dc
alu11395193 <kostenkostephan@gmail.com>
2022-04-29 09:38:57 +0200

    Control clase persona y cliente finalizado

---
+++ Tema 6/ControlFicheros/Cliente.java
@@ -0,0 +1,34 @@
+package ControlFicheros;
+
+public class Cliente extends Persona{
+    
+    private int id;
+    private String email;
+
+    public Cliente(int id,String nif, String nombre, String apellidos, String email) {
+        super(nif, nombre, apellidos);
+        this.id = id;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "id\t" + id + "\nnif\t" + nif + "\nnombre\t" + nombre + "\napellidos\t" + apellidos + "\nemail\t" + email;
+    }
+}
---
+++ Tema 6/ControlFicheros/Test.java
@@ -0,0 +1,9 @@
+package ControlFicheros;
+
+public class Test {
+    public static void main(String[] args) {
+        Cliente cl = new Cliente(1, "Y4330765V", "Stefan", "Kostenko", "kostenkostephan@gmail.com");
+    
+        System.out.println(cl.toString());
+    }
+}
