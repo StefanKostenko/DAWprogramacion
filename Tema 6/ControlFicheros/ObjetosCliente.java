8882d906d89b42fa78de673508893a8668e4b198
alu11395193 <kostenkostephan@gmail.com>
2022-05-06 09:00:30 +0200

    Crear xml de objetos finalizado

--- Tema 6/ControlFicheros/ObjetosCliente.java
+++ Tema 6/ControlFicheros/ObjetosCliente.java
@@ -8,7 +8,20 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class ObjetosCliente {
+
+    // Crear objetos apartir de arrays.
 
     private static void crearClientes() throws IOException, FileNotFoundException {
         ObjectOutputStream escritor = new ObjectOutputStream(new FileOutputStream("Tema 6/ControlFicheros/Clientes.obj"));
@@ -28,6 +41,8 @@ public class ObjetosCliente {
         escritor.close();
     }
 
+    // Leer los objetos creados.
+
     private static void leerObjetos() throws IOException, ClassNotFoundException{
 
         ObjectInputStream lector = new ObjectInputStream(new FileInputStream("Tema 6/ControlFicheros/Clientes.obj"));
@@ -45,9 +60,45 @@ public class ObjetosCliente {
         }
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException{
+    // Crear xml a partir de objetos.
+
+    private static void crearClientesXML() throws IOException, ClassNotFoundException, ParserConfigurationException, TransformerException{
+        ObjectInputStream lector = new ObjectInputStream(new FileInputStream("Tema 6/ControlFicheros/Clientes.obj"));
+
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        Element root = doc.createElement("clientes");
+        doc.appendChild(root);
+
+        Cliente c;
+        try{
+            while(true){
+                c = (Cliente) lector.readObject();
+                Element emp = doc.createElement("cliente");
+                emp.setAttribute("id", Integer.toString(c.getId()));
+
+                Element hijo = doc.createElement("nombre");
+                hijo.appendChild(doc.createTextNode(c.getNombre()));
+                emp.appendChild(hijo);
+
+                root.appendChild(emp);
+            }
+        } catch (EOFException eof) {
+            lector.close();
+        }
+
+        Transformer transformar = TransformerFactory.newInstance().newTransformer();
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult resultado = new StreamResult(new FileOutputStream("Tema 6/ControlFicheros/Clientes.xml"));
+
+        transformar.transform(source, resultado);
+        
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, ParserConfigurationException, TransformerException{
         crearClientes();
         leerObjetos();
+        crearClientesXML();
     }
     
 }
