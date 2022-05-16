d27cd62d29561cd08e5e0fa70aba3f8d355e5f27
alu11395193 <kostenkostephan@gmail.com>
2022-05-16 12:11:11 +0200

    Calculadora hecha

--- Tema 6/Web/LeerWeb.java
+++ Tema 6/Web/LeerWeb.java
@@ -1,21 +1,14 @@
 package Web;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import javax.sound.sampled.Line;
 
 public class LeerWeb {
 
