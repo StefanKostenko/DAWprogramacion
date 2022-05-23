102cc61054364b23a3b1bd1a12e421d159de547d
alu11395193 <kostenkostephan@gmail.com>
2022-05-23 08:30:17 +0200

    Usuario hehco

---
+++ Tema 6/Agenda/Agenda.java
@@ -0,0 +1,16 @@
+package Agenda;
+
+public class Agenda {
+    
+    public void pedirUsuarios() {
+
+    }
+
+    public void leerListadoTexto(){
+
+    }
+
+    public void leerListadoObjetos(){
+        
+    }
+}
---
+++ Tema 6/Agenda/Usuario.java
@@ -0,0 +1,49 @@
+package Agenda;
+
+public class Usuario {
+    private static final long serialVersionUID = 1L;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private int telefono;
+
+    public Usuario(String nombre, String apellidos, String email, int telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+}
