78828d0adb5fa7bbf8516d0c0c1a5aa6f20e2d67
StefanKostenko <kostenkostephan@gmail.com>
2022-05-30 08:45:30 +0200

    Consulta finaliozada

--- Tema 6/Mysql/Conexion.java
+++ Tema 6/Mysql/Conexion.java
@@ -32,9 +32,9 @@ public class Conexion {
       PreparedStatement ps = con.prepareStatement(consulta);
       ResultSet rs = ps.executeQuery();
 
-        while(rs.next()){
-          System.out.println (rs.getInt (1) + " " + rs.getString (2)+ " " + rs.getString(3) + " " + rs.getString(4));
-        }
+      while(rs.next()){
+        System.out.println (rs.getInt (1) + " " + rs.getString (2)+ " " + rs.getString(3) + " " + rs.getString(4));
+      }
 
       con.close();
 
