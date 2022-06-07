5fea70a961d90e7ce51aa8cfa9d2af71f1c213fd
StefanKostenko <kostenkostephan@gmail.com>
2022-06-07 22:50:32 +0200

    Por finalizar coches leer objc  funciona

---
+++ Repaso/Coches/Coches.java
@@ -0,0 +1,104 @@
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.io.Serializable;
+
+import javax.tools.FileObject;
+
+public class Coches implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    private String matricula;
+    private String marca;
+    private double tamanyoDeposito;
+    private String modelo;
+
+    public Coches(String matricula, String marca, double tamañoDeposito, String modelo) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.tamanyoDeposito = tamañoDeposito;
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getTamañoDeposito() {
+        return tamanyoDeposito;
+    }
+
+    public void setTamañoDeposito(double tamañoDeposito) {
+        this.tamanyoDeposito = tamañoDeposito;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+        
+    @Override
+    public String toString() {
+        return "marca\n" + marca + "\nmatricula\n" + matricula + "\nmodelo\n" + modelo + "\ntamaño Deposito\n"
+                + tamanyoDeposito;
+    }
+    
+    public void darAltaCoche() throws FileNotFoundException, IOException{
+
+        ObjectOutputStream escrito = new ObjectOutputStream(new FileOutputStream("Repaso/Coches/coches.txt", true));
+
+        try{
+            escrito.writeObject(this);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        escrito.close();
+        escrito.reset();
+    }
+
+    public static void leerCochesDeAlta()  throws IOException, ClassNotFoundException{
+       
+        ObjectInputStream lector = new ObjectInputStream(new FileInputStream("Repaso/Coches/coches.txt"));
+
+        Coches c;
+        
+        try{
+            
+            while(true){
+                c = (Coches) lector.readObject();
+
+                System.out.println("marca\t" + c.getMarca() + "\tmatricula\t" + c.getMatricula() + "\tmodelo\t" + c.getModelo() + "\ttamaño deposito\t" + c.getTamañoDeposito());
+            }
+        }catch (EOFException eof){
+            lector.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+        leerCochesDeAlta();
+    }
+}
